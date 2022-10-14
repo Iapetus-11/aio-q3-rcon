98fd11140c210ba18c053076294ad4d23d112db7
Iapetus-11 <Iapetus-11@users.noreply.github.com>
2022-10-14 12:29:41 +0000

    format / fix code

--- examples/basic.py
+++ examples/basic.py
@@ -4,7 +4,7 @@ from quake3_rcon import Client
 
 
 async def main():
-    async with Client('example.com', 27960, 'password') as client:
+    async with Client("example.com", 27960, "password") as client:
         while True:
             command = input("> ")
             print(await client.send_command(command))
